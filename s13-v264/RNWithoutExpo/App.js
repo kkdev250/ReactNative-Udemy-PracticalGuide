1c3a8862c18d113c57a269fd060145920f00aea4
Karol Kopicera <karol.kopicera@softwarehut.com>
2022-02-12 15:17:49 +0100

    start of 267. Adding Native Modules to Non-Expo Apps

---
+++ s13-v264/RNWithoutExpo/App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  View,
+  StyleSheet,
+  Button,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Button title="Take Image" onPress={() => {}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+export default App;
