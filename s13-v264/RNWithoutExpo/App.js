a4051eeaadf9b317f7a0636a74e2a75dd21a5115
Karol Kopicera <karol.kopicera@softwarehut.com>
2022-02-12 15:57:59 +0100

    after 267. Adding Native Modules to Non-Expo Apps

--- s13-v264/RNWithoutExpo/App.js
+++ s13-v264/RNWithoutExpo/App.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+import ImagePicker from 'react-native-image-picker';
 
 import {
   View,
@@ -15,9 +16,38 @@ import {
 } from 'react-native';
 
 const App = () => {
+  const pickImage = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+    /**
+     * The first arg is the options object for customization (it can also be null or omitted for default options),
+     * The second arg is the callback which sends object: response (more info in the API Reference)
+     */
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        console.log(response.uri);
+      }
+    });
+  }
+
   return (
     <View>
-      <Button title="Take Image" onPress={() => {}} />
+      <Button title="Take Image" onPress={pickImage} />
     </View>
   );
 };
