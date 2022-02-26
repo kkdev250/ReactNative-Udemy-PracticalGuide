8dc2c9d6ec4adefcad484f8f63a15cc255e888ad
Karol Kopicera <karol.kopicera@softwarehut.com>
2022-02-26 12:44:45 +0100

    after 310. More on Push Tokens

--- s16-v300/react-native-push/App.js
+++ s16-v300/react-native-push/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Button, View, Alert } from 'react-native';
 import * as Notifications from 'expo-notifications';
 
@@ -14,16 +14,29 @@ Notifications.setNotificationHandler({ //configuring notifications when app is f
 })
 
 export default function App() {
-  useEffect(() => { //asking for permissions - only for ios!
+  const [pushToken, setPushToken] = useState();
+
+  useEffect(() => { 
     const permissionsHandler = async () => {
-      let permissions = await Notifications.getPermissionsAsync();
-      if (!permissions.granted) {
-        await Notifications.requestPermissionsAsync();
-      }
-      permissions = await Notifications.getPermissionsAsync();
-      if (!permissions.granted) {
-        Alert.alert('Error', 'Notification permissions should be allowed', [{text: 'Okay'}]);
-        return;
+      try {
+        let permissions = await Notifications.getPermissionsAsync(); //asking for permissions - important only for ios!
+        if (!permissions.granted) {                                  //...for Android is always granted
+          await Notifications.requestPermissionsAsync();
+        }
+        permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+          Alert.alert('Error', 'Notification permissions should be allowed', [{text: 'Okay'}]);
+          throw new Error('Permission not granted!');
+        }
+        console.log('Getting token') //get push notification token (for this device) from Expo... 
+        const response = await Notifications.getExpoPushTokenAsync();
+        console.log(response)
+        const token = response.data;
+        setPushToken(token);
+        //fetch('https://your-own-api.com'...) each app could send its token to the backend to create a all tokens database for sending notifications
+      } catch(err) {
+        console.log(err);
+        return null;
       }
     }
 
@@ -49,7 +62,7 @@ export default function App() {
     };
   }, []);
 
-  const triggerNotificationHandler = () => {
+  const triggerLocalNotificationHandler = () => {
     Notifications.scheduleNotificationAsync({ //create a local notification
       content: {
         title: 'My first local notification',
@@ -62,11 +75,32 @@ export default function App() {
     }); 
   };
 
+  const triggerPushNotificationHandler = () => {
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Accept-Encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        to: pushToken,
+        data: {extraData: 'Some data'},
+        title: 'Sent via the app',
+        body: 'This push notification was sent via the app!'
+      }),
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Button
-        title="Trigger Notification" //remember to hide the app to background to see the notification!
-        onPress={triggerNotificationHandler}
+        title="Trigger Local Notification" //remember to hide the app to background to see the notification!
+        onPress={triggerLocalNotificationHandler}
+      />
+      <Button
+        title="Trigger Push Notification"
+        onPress={triggerPushNotificationHandler}
       />
       <StatusBar style="auto" />
     </View>
@@ -78,6 +112,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
 });
